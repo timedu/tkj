a9000efd5a1731b235ab045442525ec3569c153a
TiM <tim@dyn-178-38.public.tut.fi>
2016-04-21 09:43:27 +0300

    basic setup

--- js/config.js
+++ js/config.js
@@ -53,8 +53,8 @@ $(function() {
 
      // If Github mode is set, your Github username and repo name.
     githubUserSettings: {
-      username: 'yourusername',
-      repo: 'yourrepo'
+      username: 'timedu',
+      repo: 'tkj'
     },
 
     // If Github mode is set, choose which Github branch to get files from.
